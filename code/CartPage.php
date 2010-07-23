2a824a7b7ea7915cfdd955b01584cbd66d19761b
Nicolaas Francken <github@sunnysideup.co.nz>
2010-07-23 05:13:48 +0000

    some code style improvements in CartPage.php

--- code/CartPage.php
+++ code/CartPage.php
@@ -1,47 +1,49 @@
 <?php
 
 class CartPage extends Page{
-	
+
 	static $db = array();
-	
+
 	static $has_one = array(
 		'CheckoutPage' => 'CheckoutPage',
 		'ContinuePage' => 'SiteTree'
 	);
-	
+
 	static $icon = 'ecommerce/images/icons/cart';
-	
+
 	function getCMSFields(){
 		$fields = parent::getCMSFields();
-		if($checkouts = DataObject::get('CheckoutPage'))
+		if($checkouts = DataObject::get('CheckoutPage')) {
 			$fields->addFieldToTab('Root.Content.Links',new DropdownField('CheckoutPageID','Checkout Page',$checkouts->toDropDownMap()));
-		if($pgroups = DataObject::get('ProductGroup'))
+		}
+		if($pgroups = DataObject::get('ProductGroup')) {
 			$fields->addFieldToTab('Root.Content.Links',new DropdownField('ContinuePageID','Continue Product Group Page',$pgroups->toDropDownMap()));
-		
+		}
+
 		return $fields;
 	}
-	
+
 }
 
 class CartPage_Controller extends Page_Controller{
-		
+
 		function Order() {
 		if($orderID = Director::urlParam('Action')) return DataObject::get_by_id('Order', $orderID);
 		else return ShoppingCart::current_order();
 		}
-	
+
 		public function init() {
 		// include extra js requirements for this page
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
 		Requirements::javascript('ecommerce/javascript/CheckoutPage.js');
-				
+
 		// include stylesheet for the checkout page
 		Requirements::themedCSS('CheckoutPage');
-		
+
 		parent::init();
 
 	}
 }
 
 
-?>
+
