349e7a8202f7875ae277414016020a716714ade4
Jeremy Shipman <jeremy@burnbright.net>
2010-08-17 06:36:36 +0000

    bulk loader improvements

--- code/cms/ProductBulkLoader.php
+++ code/cms/ProductBulkLoader.php
@@ -5,8 +5,6 @@ class ProductBulkLoader extends CsvBulkLoader{
 	static $parentpageid = null;
 	
 	public $columnMap = array(
-		'ProductID' => '->importClassName'
-		
 		//'Image' => '->linkToImage'//TODO: set image, based on filename (perhaps all product images could go in assets/products/)
 	);
 	
@@ -16,7 +14,6 @@ class ProductBulkLoader extends CsvBulkLoader{
 	
 	public $relationCallbacks = array();
 	
-	
 	protected function processAll($filepath, $preview = false) {
 		$results = parent::processAll($filepath, $preview);
 		
@@ -25,33 +22,31 @@ class ProductBulkLoader extends CsvBulkLoader{
 		$objects->merge($results->Created());
 		$objects->merge($results->Updated());
 		foreach($objects as $object){
+			
+			$object->ClassName = "Product";
+			 //TODO: find or create parent category, if provided
+			
+			if(!$object->ParentID){
+				 //set parent page
+				if(self::$parentpageid instanceof ProductGroup) //cached option
+					$object->ParentID = self::$parentpageid;
+				elseif($parentpage = DataObject::get_one('ProductGroup',"Title = 'Products'",'Created DESC')){ //page called 'Products'
+					$object->ParentID = self::$parentpageid = $parentpage->ID;
+				}elseif($parentpage = DataObject::get_one('ProductGroup',"ParentID = 0",'Created DESC')){ //root page
+					$object->ParentID = self::$parentpageid = $parentpage->ID;
+				}elseif($parentpage = DataObject::get_one('ProductGroup',"",'Created DESC')){ //any product page
+					$object->ParentID = self::$parentpageid = $parentpage->ID;
+				}else
+					$object->ParentID = self::$parentpageid = 0;
+			}
+			
 			$object->writeToStage('Stage'); 
 			$object->publish('Stage', 'Live');
 		}
 		return $results;
 	}
 	
-	
-	//Hack to make custom changes to new objects
-	static function importClassName(&$obj, $val, $record){
-		
-		$obj->ClassName = "Product";
-		
-		 //TODO: find or create parent category, if provided
-		
-		 //set parent page
-		if(self::$parentpageid instanceof ProductGroup) //cached option
-			$obj->ParentID = self::$parentpageid;
-		elseif($parentpage = DataObject::get_one('ProductGroup',"Title = 'Products'",'Created DESC')){ //page called 'Products'
-			$obj->ParentID = self::$parentpageid = $parentpage->ID;
-		}elseif($parentpage = DataObject::get_one('ProductGroup',"ParentID = 0",'Created DESC')){ //root page
-			$obj->ParentID = self::$parentpageid = $parentpage->ID;
-		}elseif($parentpage = DataObject::get_one('ProductGroup',"",'Created DESC')){ //any product page
-			$obj->ParentID = self::$parentpageid = $parentpage->ID;
-		}else
-			$obj->ParentID = self::$parentpageid = 0;
-		
-	}
+
 	
 }
 
