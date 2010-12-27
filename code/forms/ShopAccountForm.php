e95be8541c67b876dabe694d465251a0a544ae39
Nicolaas Francken <github@sunnysideup.co.nz>
2010-12-27 04:26:17 +0000

    added a line in if else... in ShopAccountForm.php

--- code/forms/ShopAccountForm.php
+++ code/forms/ShopAccountForm.php
@@ -9,7 +9,7 @@ class ShopAccountForm extends Form {
 
 	function __construct($controller, $name) {
 		$member = Member::currentUser();
-		
+
 		$requiredFields = null;
 
 		if($member && $member->exists()) {
@@ -19,15 +19,16 @@ class ShopAccountForm extends Form {
 			if($member->Password != '') {
 				$passwordField->setCanBeEmpty(true);
 			}
-			
+
 			//TODO:is this necessary?
 			$fields->push(new LiteralField('LogoutNote', "<p class=\"message warning\">" . _t("MemberForm.LOGGEDIN","You are currently logged in as ") . $member->FirstName . ' ' . $member->Surname . ". "._t('MemberForm.LOGOUT','Click <a href="Security/logout">here</a> to log out.')."</p>"));
-			
+
 			$fields->push(new HeaderField('Login Details',_t('MemberForm.LOGINDETAILS','Login Details'), 3));
 			$fields->push($passwordField);
 
 			$requiredFields = new ShopAccountFormValidator($member->getEcommerceRequiredFields());
-		} else {
+		}
+		else {
 			$fields = new FieldSet();
 		}
 
@@ -46,14 +47,14 @@ class ShopAccountForm extends Form {
 		}
 
 		parent::__construct($controller, $name, $fields, $actions, $requiredFields);
-		
-		
+
+
 		if($member){
-			$member->Password = ""; //prevents password field from being populated with encrypted password data 
+			$member->Password = ""; //prevents password field from being populated with encrypted password data
 			$this->loadDataFrom($member);
 		}
-		
-		
+
+
 	}
 
 	/**
