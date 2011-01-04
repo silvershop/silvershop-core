40149f912c17a171726bcbda255fcb15a345a1ef
Nicolaas Francken <github@sunnysideup.co.nz>
2011-01-04 15:41:35 +0000

    SiteConfigEcommerceExtras: added DispatchEmailSubject, changed fields arounds...

--- code/dod/SiteConfigEcommerceExtras.php
+++ code/dod/SiteConfigEcommerceExtras.php
@@ -14,6 +14,7 @@ class SiteConfigEcommerceExtras extends DataObjectDecorator {
 				"ShopClosed" => "Boolean",
 				"ReceiptEmail" => "Varchar(255)",
 				"ReceiptSubject" => "Varchar(255)",
+				"DispatchEmailSubject" => "Varchar(255)",
 				"PostalCodeURL" => "Varchar(255)",
 				"PostalCodeLabel" => "Varchar(255)"
 			)
@@ -21,11 +22,14 @@ class SiteConfigEcommerceExtras extends DataObjectDecorator {
 	}
 
 	function updateCMSFields(FieldSet &$fields) {
+		$fields->addFieldToTab("Root.Webshop", new HeaderField("Checkout", "Checkout", 3));
 		$fields->addFieldToTab("Root.Webshop", new CheckboxField("ShopClosed", "Shop closed"));
-		$fields->addFieldToTab("Root.Webshop", new EmailField("ReceiptEmail", "From email address for shop receipt (e.g. sales@myshop.com)"));
-		$fields->addFieldToTab("Root.Webshop", new TextField("ReceiptSubject", "Subject for shop receipt email ('{OrderNumber}' will be replaced with actual order number - e.g. 'thank you for your order (#{OrderNumber})');"));
 		$fields->addFieldToTab("Root.Webshop", new TextField("PostalCodeLink", "Postal code link"));
 		$fields->addFieldToTab("Root.Webshop", new TextField("PostalCodeLabel", "Postal code label"));
+		$fields->addFieldToTab("Root.Webshop", new HeaderField("Emails", "Confirmation Emails", 3));
+		$fields->addFieldToTab("Root.Webshop", new EmailField("ReceiptEmail", "From email address for shop receipt (e.g. sales@myshop.com)"));
+		$fields->addFieldToTab("Root.Webshop", new TextField("ReceiptSubject", "Subject for shop receipt email ('{OrderNumber}' will be replaced with actual order number - e.g. 'thank you for your order (#{OrderNumber})');"));
+		$fields->addFieldToTab("Root.Webshop", new TextField("DispatchEmailSubject", "Default subject for dispatch email (e.g. your order has been sent)"));
 		return $fields;
 	}
 
