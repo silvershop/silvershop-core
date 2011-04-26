bbdeddbb1f7f075d9ebc431b21f79d578b90b339
Nicolaas Francken <github@sunnysideup.co.nz>
2011-04-26 14:26:14 +0000

    Order_ReceiptEmail.php: added comments

--- code/email/Order_ReceiptEmail.php
+++ code/email/Order_ReceiptEmail.php
@@ -2,9 +2,13 @@
 
 /**
  * @Description: This class handles the receipt email which gets sent once an order is made.
- ** You can call it by issuing sendReceipt() in the Order class.
- * @package: ecommerce
+ * You can call it by issuing sendReceipt() in the Order class.
+ *
  * @authors: Silverstripe, Jeremy, Nicolaas
+ *
+ * @package: ecommerce
+ * @sub-package: email
+ *
  **/
 
 class Order_ReceiptEmail extends Order_Email {
