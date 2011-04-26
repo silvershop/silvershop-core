923a052898cd5b9883cf4668bef66c44c18f88aa
Nicolaas Francken <github@sunnysideup.co.nz>
2011-04-26 14:09:46 +0000

    EcommerceResponse.php: added comments

--- code/control/EcommerceResponse.php
+++ code/control/EcommerceResponse.php
@@ -1,7 +1,12 @@
 <?php
 
 /**
+ * @description: This class is a base class for Ecommerce Responses such as Cart Response
+ *
  * @authors: Jeremy, Nicolaas
+ *
+ * @package: ecommerce
+ * @sub-package: control
  *
  **/
 abstract class EcommerceResponse extends SS_HTTPResponse {
