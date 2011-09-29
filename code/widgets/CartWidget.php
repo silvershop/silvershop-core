caaf10bfd2f9212f1df5c46cc1b6260a4303cd37
Jeremy Shipman <jeremy@burnbright.net>
2011-09-29 00:12:17 +0000

    0.8: added missing cart widget template

--- code/widgets/CartWidget.php
+++ code/widgets/CartWidget.php
@@ -9,7 +9,7 @@ class CartWidget extends Widget{
 
 	static $title = "Shopping Cart";
 	static $cmsTitle = "Shopping Cart";
-	static $description = "displays the current contents of the user's cart";
+	static $description = "Displays the current contents of the user's cart.";
 
 	function Cart(){
 		return ShoppingCart::current_order();
