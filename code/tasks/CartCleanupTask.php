db1452ffcb15bbff2fcc2f7be33dc8964ca38d05
Jeremy Shipman <jeremy@burnbright.net>
2011-11-08 23:21:44 +0000

    MINOR: layout change of task output

--- code/tasks/CartCleanupTask.php
+++ code/tasks/CartCleanupTask.php
@@ -1,25 +1,27 @@
 <?php
 
 class CartCleanupTask extends WeeklyTask{
-	
+
 	static $cleardays = 90;
-	
+
 	function set_clear_days($days = 90){
 		self::$cleardays = $days;
 	}
-	
+
 	//Find and remove carts older than X days
 	function process(){
-		
+
 		$time = date('Y-m-d H:i:s', strtotime("-".self::$cleardays." days"));
 		if($oldcarts = DataObject::get('Order',"\"Status\" = 'Cart' AND \"LastEdited\" < '$time'")){
+			echo "<br/>\nDeleted ids: ";
+
 			foreach($oldcarts as $cart){
-				echo "deleting ".$cart->Title."  </br>\n";
+				echo $cart->Title." ";
 				$cart->delete();
 				$cart->destroy();
 			}
 		}
-		
-	} 
-	
+
+	}
+
 }
