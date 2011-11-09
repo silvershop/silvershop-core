c45f6c762134ff3590f5af53c2a3ed9d3451bfb7
Jeremy Shipman <jeremy@burnbright.net>
2011-11-09 04:31:27 +0000

    MINOR improvements to RecalculatedOrderTask

--- code/tasks/RecalculateAllOrdersTask.php
+++ code/tasks/RecalculateAllOrdersTask.php
@@ -7,25 +7,31 @@
 class RecalculateAllOrdersTask extends BuildTask {
 
 	protected $title = "Recalculate All Orders";
-
 	protected $description = "Runs all price calculation functions on all orders.";
 
 	function run($request){
+		$br = Director::is_cli() ? "\n" : "<br/>";
+		$verbose = true;
 
 		//TODO: include order total calculation, once that gets written
+		//TODO: figure out how to make this run faster
+		//TODO: better memory managment...the destroy calls are not enough it appears.
 
 		if($orders = DataObject::get("Order")){
-			echo "<br/>\nWriting all order items: ";
+			echo $br."Writing all order items ";
 			foreach($orders as $order){
 				if($items = $order->Items()){
 					foreach($items as $item){
 						if($item->Product()){
-							echo $item->ID." ";
+							if($verbose) echo $item->ID." ";
 							$item->write(); //OrderItem->onBeforeWrite calls 'CalculateTotal'
 						}
+						$item->destroy();
 					}
 				}
+				$order->destroy();
 			}
+			echo $br."done.".$br;
 		}
 	}
 
