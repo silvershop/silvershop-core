cdc59d52f1c5d5813cd4acb2a3592d5293477567
Jeremy Shipman <jeremy@burnbright.net>
2012-04-23 13:28:24 +1200

    ENHANCEMENT: created AddProductForm for adding products via a form submission. This should help with preventing carts from being created for no reason. Relates to #7

---
+++ code/forms/AddProductForm.php
@@ -0,0 +1,32 @@
+<?php
+
+class AddProductForm extends Form{
+	
+	function __construct($controller,$product){
+		
+		$fields = new FieldSet(
+			new NumericField("Quantity",_t("AddProductForm.QUANTITY","Quantity"),1),
+			new HiddenField("ProductID","",$product->ID),
+			new LiteralField("Price", "<h2 class=\"price\">".$product->dbObject('Price')->Nice()."</h2>")
+		);
+		$actions = new FieldSet(
+			new FormAction('addtocart',_t("AddProductForm.ADDTOCART",'Add to Cart'))
+		);
+		
+		$validator = new RequiredFields(array(
+			'Quantity',
+			'ProductID'
+		));
+		
+		parent::__construct($controller,"AddProductForm",$fields,$actions,$validator);
+	}
+	
+	function addtocart($data,$form){
+		$product = DataObject::get_by_id('Product',(int) $data['ProductID']);
+		$cart = ShoppingCart::getInstance();
+		$cart->add($product,(int) $data['Quantity'],$data);
+		$form->SessionMessage($cart->getMessage(),$cart->getMessageType());
+		Director::redirectBack();
+	}
+	
+}
