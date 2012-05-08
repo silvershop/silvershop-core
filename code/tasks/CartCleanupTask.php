953ee2b80763082b1514e444019c8c3db4eea1b6
Jeremy Shipman <jeremy@burnbright.net>
2012-05-08 15:43:35 +1200

    ENHANCEMENT: Introduced optional SQL-based delete for cart cleanup task. Its less safe, but it's faster.

--- code/tasks/CartCleanupTask.php
+++ code/tasks/CartCleanupTask.php
@@ -5,33 +5,83 @@
  * @package shop
  * @subpackage tasks
  */
-class CartCleanupTask extends WeeklyTask{
+class CartCleanupTask extends BuildTask{
+	
+	static $batch_size = 500;
+	protected static $cleardays = 90;
 
-	static $cleardays = 90;
-
+	protected $title = "Delete Old Carts";
+	protected $description = "Deletes carts that are older than a certian number of days (default: 90).
+									Add type=sql to use a faster, but less safe query.";
+	
 	function set_clear_days($days = 90){
 		self::$cleardays = $days;
 	}
 
 	//Find and remove carts older than X days
-	function process(){
-
+	function run($request){
+		if(strtolower($request->getVar('type')) == 'sql'){
+			$this->sqldelete();
+		}else{
+			$this->ormdelete();
+		}
+	}
+	
+	/**
+	 * Perform delete via SQL commands, bypassing PHP.
+	 * Fast, but may miss some custom cleanup or may delete records linked to other data.
+	 */
+	function sqldelete(){
+		//delete carts older than 90 days
+		$days = self::$cleardays;
+		$filter = ($days) ? "AND \"Order\".\"LastEdited\" < ADDDATE(NOW(),INTERVAL -$days DAY)" : "";
+		$deleteorders = new SQLQuery("*","\"Order\"","\"Order\".\"Status\" = 'Cart' $filter");
+		$deleteorders->delete = true;
+		$result = $deleteorders->execute();
+		echo "Deleted ".(int)$result->numRecords()." Order.\n<br/>";
+		
+		//delete orphaned attributes
+		$attributeclass = "OrderAttribute";
+		$join = "LEFT JOIN \"Order\" ON \"OrderAttribute\".\"OrderID\" = \"Order\".\"ID\"";
+		$where = "\"Order\".\"ID\" IS NULL";
+		$from = "\"OrderAttribute\" $join";
+		$result = DB::query("DELETE $attributeclass FROM $from WHERE $where"); //Can't use SQLQuery for joined deletes, because of a bug with SQLQuery
+		echo "Deleted ".(int)$result->numRecords()." $attributeclass.\n<br/>";
+		 
+		//delete orphaned subclasses of OrderAttribute
+		foreach(ClassInfo::dataClassesFor($attributeclass) as $dataclass){
+			if(!$dataclass || $dataclass == $attributeclass) continue;
+			$from = "\"$dataclass\" LEFT JOIN \"$attributeclass\" ON \"$dataclass\".\"ID\" = \"$attributeclass\".\"ID\"";
+			$where = "\"$attributeclass\".\"ID\" IS NULL";
+			$result = DB::query("DELETE $dataclass FROM $from WHERE $where"); //Can't use SQLQuery for joined deletes, because of a bug with SQLQuery
+		 	echo "Deleted ".(int)$result->numRecords()." $dataclass.\n<br/>";
+		}
+	}
+	
+	/**
+	 * Delete via standard ORM delete functions.
+	 * This will trigger any onBeforeDelete or onAfterDelete calls.
+	 */
+	function ormdelete(){
+		$start = 0;
+		$count = 0;
 		$time = date('Y-m-d H:i:s', strtotime("-".self::$cleardays." days"));
 		$filter = "\"Status\" = 'Cart'";
-		
 		if(self::$cleardays){
 			$filter .= " AND \"LastEdited\" < '$time'";
 		}
-		
-		if($oldcarts = DataObject::get('Order',$filter)){
-			echo "<br/>\nDeleted ids: ";
-			foreach($oldcarts as $cart){
-				echo $cart->Title." ";
+		while($batch = DataObject::get('Order',$filter,"\"Created\" ASC","",$start.",".self::$batch_size)){
+			foreach($batch as $cart){
+				echo ". ";
 				$cart->delete();
 				$cart->destroy();
+				$count++;
 			}
-		}
-
+			$start += self::$batch_size;
+			echo "$count old carts removed.\n<br/>";
+		};
 	}
+	
+
 
 }
