a305dea2bc2c539c368ee2cc1f75c51ee8aea9b7
Jeremy Shipman <jeremy@burnbright.net>
2012-08-09 11:24:37 +1200

    API: added Sum function to OrderItemList (ComponentSet Decorator), which allows summing any field on an OrderItem.

--- code/model/OrderItemList.php
+++ code/model/OrderItemList.php
@@ -6,17 +6,21 @@
 class OrderItemList extends Extension{
 	
 	function Quantity(){
-		$quantity = 0;
-		if($this->owner->exists()){
-			foreach($this->owner as $item){
-				$quantity += $item->Quantity;
-			}
-		}
-		return $quantity;
+		return $this->owner->Sum('Quantity');
 	}
 	
 	function Plural(){
 		return $this->Quantity() > 1;
+	}
+	
+	function Sum($field){
+		$total = 0;
+		if($this->owner->exists()){
+			foreach($this->owner as $item){
+				$total += $item->$field;
+			}
+		}
+		return $total;
 	}
 	
 }
