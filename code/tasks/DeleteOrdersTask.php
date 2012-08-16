8dffaf71ca5a278ad584b7b3ed66b32255d65d0f
Jeremy Shipman <jeremy@burnbright.net>
2012-08-16 16:46:49 +1200

    Refactored delete orders task to separate sql deletion from orm deletion, making the task a bit safer.

--- code/tasks/DeleteOrdersTask.php
+++ code/tasks/DeleteOrdersTask.php
@@ -10,36 +10,44 @@ class DeleteOrdersTask extends BuildTask{
 	protected $title = "Delete Orders";
 	protected $description = "Deletes all orders, order items, and payments from the database.";
 
-	function run($request){
+	function run($request = null){
+		if($request && $request->getVar('type') == "sql"){
+			$this->sqldelete();
+		}else{
+			$this->ormdelete();
+		}
+	}
 
+	function ormdelete(){
 		if($allorders = DataObject::get('Order')){
 			foreach($allorders as $order){
-				//TODO: delete member(s)?
 				$order->delete();
 				$order->destroy();
 			}
 		}
+	}
 
+	function sqldelete(){
 		$basetables = array(
 			'Order',
 			'OrderAttribute',
 			'OrderStatusLog',
 			'Payment'
 		);
-
+		
 		foreach($basetables as $table){
 			if(!(ClassInfo::hasTable($table))) continue;
-
+		
 			foreach(ClassInfo::subclassesFor($table) as $key => $class){
 				//TODO: empty all pivot(many_many) tables on this side of relationship
-
+				//TODO: only delete payments that have an orderid
+		
 				if(ClassInfo::hasTable($class)){
-					DB::query("DELETE FROM \"$class\" WHERE 1;");
 					echo "<p>Deleting all $class</p>";
+					DB::query("DELETE FROM \"$class\" WHERE 1;");
 				}
 			}
 		}
-
 	}
 
 }
