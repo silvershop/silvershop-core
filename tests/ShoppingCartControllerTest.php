30b75cae0c31cd543eba8c13aed663ee75164c23
Jeremy Shipman <jeremy@burnbright.net>
2012-10-16 17:11:54 +1300

    BUG: ShoppingCartControllerTest fix and tidy

--- tests/ShoppingCartControllerTest.php
+++ tests/ShoppingCartControllerTest.php
@@ -33,7 +33,6 @@ class ShoppingCartControllerTest extends FunctionalTest {
 	}
 
 	function testAddToCart(){
-
 		// add 2 of the same items via url
 		$this->get(ShoppingCart_Controller::add_item_link($this->mp3player)); //add item via url
 		$this->get(ShoppingCart_Controller::add_item_link($this->mp3player)); //add another
@@ -44,30 +43,28 @@ class ShoppingCartControllerTest extends FunctionalTest {
 
 		// See what's in the cart
 		$items = ShoppingCart::get_items();
-
 		$this->assertNotNull($items);
-		if($items){
-			$this->assertEquals($items->Count(), 2, 'There are 2 items in the cart');
-			$mp3playeritem = $items->find('ProductID',$this->mp3player->ID);
 
-			/* We have the product that we asserted in our fixture file, with a quantity of 2 in the cart */
-			$this->assertEquals($mp3playeritem->ProductID, $this->mp3player->ID, 'We have the correct Product ID in the cart.');
-			$this->assertEquals($mp3playeritem->Quantity, 2, 'We have 2 of this product in the cart.');
+		$this->assertEquals($items->Count(), 2, 'There are 2 items in the cart');
+		$mp3playeritem = $items->find('ProductID',$this->mp3player->ID);
 
-			/* set item quantiy */
-			$this->get(ShoppingCart_Controller::set_quantity_item_link($this->mp3player,array('quantity' => 5))); //add item via url
-			$items = ShoppingCart::get_items();
-			$mp3playeritem = $items->find('ProductID',$this->mp3player->ID);
-			$this->assertEquals($mp3playeritem->Quantity, 5, 'We have 5 of this product in the cart.');
+		/* We have the product that we asserted in our fixture file, with a quantity of 2 in the cart */
+		$this->assertEquals($mp3playeritem->ProductID, $this->mp3player->ID, 'We have the correct Product ID in the cart.');
+		$this->assertEquals($mp3playeritem->Quantity, 2, 'We have 2 of this product in the cart.');
 
-			/* non purchasable product checks */
-			$this->assertEquals($this->noPurchaseProduct->canPurchase(),false,'non-purcahseable product is not purchaseable');
-			$this->assertArrayNotHasKey($this->noPurchaseProduct->ID,$items->map('ProductID'),'non-purcahable product is not in cart');
-			$this->assertEquals($this->draftProduct->canPurchase(),false,'draft product is not purchaseable');
-			$this->assertArrayNotHasKey($this->draftProduct->ID,$items->map('ProductID'),'draft product is not in cart');
-			$this->assertEquals($this->noPriceProduct->canPurchase(),false,'product without price is not purchaseable');
-			$this->assertArrayNotHasKey($this->noPriceProduct->ID,$items->map('ProductID'),'product without price is not in cart');
-		}
+		/* set item quantiy */
+		$this->get(ShoppingCart_Controller::set_quantity_item_link($this->mp3player,array('quantity' => 5))); //add item via url
+		$items = ShoppingCart::get_items();
+		$mp3playeritem = $items->find('ProductID',$this->mp3player->ID);
+		$this->assertEquals($mp3playeritem->Quantity, 5, 'We have 5 of this product in the cart.');
+
+		/* non purchasable product checks */
+		$this->assertEquals($this->noPurchaseProduct->canPurchase(),false,'non-purcahseable product is not purchaseable');
+		$this->assertArrayNotHasKey($this->noPurchaseProduct->ID,$items->map('ProductID'),'non-purcahable product is not in cart');
+		$this->assertEquals($this->draftProduct->canPurchase(),false,'draft product is not purchaseable');
+		$this->assertArrayNotHasKey($this->draftProduct->ID,$items->map('ProductID'),'draft product is not in cart');
+		$this->assertEquals($this->noPriceProduct->canPurchase(),false,'product without price is not purchaseable');
+		$this->assertArrayNotHasKey($this->noPriceProduct->ID,$items->map('ProductID'),'product without price is not in cart');
 		
 		$this->cart->clear();
 	}
