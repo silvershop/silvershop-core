3b8f7de36682a15d254a77c7d1de2e1e3c31f690
Jeremy Shipman <jeremy@burnbright.net>
2012-11-13 19:20:40 +1300

    NEW: introduced a RestrictionRegionCountryDropdownField for use within TableFields

---
+++ code/forms/fields/RestrictionRegionCountryDropdownField.php
@@ -0,0 +1,14 @@
+<?php
+
+class RestrictionRegionCountryDropdownField extends CountryDropdownField{
+	
+	function __construct($name, $title = null, $source = null, $value = "", $form = null) {
+		
+		$source = SiteConfig::current_site_config()->getCountriesList();
+		
+		parent::__construct($name, $title, $source, $value, $form);
+		$this->setHasEmptyDefault(true);
+		$this->setEmptyString("-- International --");
+	}
+	
+}
--- code/model/RegionRestriction.php
+++ code/model/RegionRestriction.php
@@ -25,6 +25,23 @@ class RegionRestriction extends DataObject{
 		'PostalCode'
 	);
 	
+	static $field_labels = array(
+		'Country' => 'Country',
+		'State' => 'State/Region',
+		'City' => 'City/Sub-Region',
+		'PostalCode' => 'Post/Zip Code'
+	);
+	
+	/*
+	 * Specifies form field types to use in TableFields
+	 */
+	static $table_field_types = array(
+		'Country' => 'RestrictionRegionCountryDropdownField',
+		'State' => 'TextField',
+		'City' => 'TextField',
+		'PostalCode' => 'TextField'
+	);
+	
 	/**
 	 * Produce a SQL filter to get matching RegionRestrictions to a given address
 	 * @param Address $address
@@ -42,6 +59,10 @@ class RegionRestriction extends DataObject{
 			$where[] = "TRIM(LOWER($rr.\"$field\")) = TRIM(LOWER('".$address->$field."')) OR $rr.\"$field\" = '*' OR $rr.\"$field\" = ''";
 		}
 		return "(".implode(") AND (", $where).")";
+	}
+	
+	static function get_table_field_types(){
+		return self::$table_field_types;
 	}
 	
 	function onBeforeWrite(){
