a42cd3a59065d78192351e35771bc73205789077
Jeremy Shipman <jeremy@burnbright.net>
2012-11-13 19:18:20 +1300

    NEW: Updated PopulateShopTask to fill out some of the allowed countries

--- code/tasks/PopulateShopTask.php
+++ code/tasks/PopulateShopTask.php
@@ -104,6 +104,25 @@ class PopulateShopTask extends BuildTask{
 			DB::alteration_message('Account page \'Account\' created', 'created');
 		}
 		
+		//countries config - removes some countries
+		$siteconfig = SiteConfig::current_site_config();
+		if(empty($siteconfig->AllowedCountries)){
+			$siteconfig->AllowedCountries = 
+			"AF,AL,DZ,AS,AD,AO,AG,AR,AM,AU,AT,AZ,BS,BH,
+			BD,BB,BY,BE,BZ,BJ,BT,BO,BA,BW,BR,BN,BG,BF,BI,
+			KH,CM,CA,CV,CF,TD,CL,CN,CO,KM,CG,CR,CI,HR,CU,
+			CY,CZ,DK,DJ,DM,DO,EC,EG,SV,GQ,ER,EE,ET,FJ,FI,
+			FR,GA,GM,GE,DE,GH,GR,GD,GT,GN,GW,GY,HT,HN,HK,
+			HU,IS,IN,ID,IR,IQ,IE,IL,IT,JM,JP,JO,KZ,KE,KI,
+			KP,KR,KW,KG,LA,LV,LB,LS,LR,LY,LI,LT,LU,MG,MW,
+			MY,MV,ML,MT,MH,MR,MU,MX,FM,MD,MC,MN,MS,MA,MZ,
+			MM,NA,NR,NP,NL,NZ,NI,NE,NG,NO,OM,PK,PW,PA,PG,
+			PY,PE,PH,PL,PT,QA,RO,RU,RW,KN,LC,VC,WS,SM,ST,
+			SA,SN,SC,SL,SG,SK,SI,SB,SO,ZA,ES,LK,SD,SR,SZ,
+			SE,CH,SY,TJ,TZ,TH,TG,TO,TT,TN,TR,TM,TV,UG,UA,
+			AE,GB,US,UY,UZ,VU,VE,VN,YE,YU,ZM,ZW";
+			$siteconfig->write();
+		}
 		$this->extend("afterPopulate");
 	}
 	
