e2f00bbef7b79094cafb24915338265b8a5bea4a
Jeremy Shipman <jeremy@burnbright.net>
2012-11-27 08:57:32 +1300

    Force locale for ShopToolsTest

--- tests/ShopToolsTest.php
+++ tests/ShopToolsTest.php
@@ -9,6 +9,8 @@ class ShopToolsTest extends SapphireTest{
 	
 	function testPriceForDisplay(){
 		$dp = ShopTools::price_for_display(12345.67);
+		$dp->setCurrency("NZD");
+		$dp->setLocale("en_NZ");
 		$this->assertEquals($dp->Nice(),"$12,345.67");
 	}
 	
