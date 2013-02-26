5dd3d27074b0640bfa69e82d844fb4efa201110e
Jeremy Shipman <jeremy@burnbright.net>
2013-02-26 15:45:00 +1300

    BUG: Prevent RestrictionRegionCountryDropdownField from being set to visitor country. NEW: Allow setting default string to something other than "-- International --"

--- code/forms/fields/RestrictionRegionCountryDropdownField.php
+++ code/forms/fields/RestrictionRegionCountryDropdownField.php
@@ -1,12 +1,14 @@
 <?php
 
-class RestrictionRegionCountryDropdownField extends CountryDropdownField{
+class RestrictionRegionCountryDropdownField extends DropdownField{
+	
+	static $defaultname = "-- International --";
 	
 	function __construct($name, $title = null, $source = null, $value = "", $form = null) {
 		$source = SiteConfig::current_site_config()->getCountriesList(true);
 		parent::__construct($name, $title, $source, $value, $form);
 		$this->setHasEmptyDefault(true);
-		$this->setEmptyString("-- International --");
+		$this->setEmptyString(self::$defaultname);
 	}
 	
 }
