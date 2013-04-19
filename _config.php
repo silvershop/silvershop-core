baf79533fe5137255ae23aab8298ef9357529640
Jeremy Shipman <jeremy@burnbright.net>
2013-04-19 15:23:31 +1200

    Tidied up _config.php file.

--- _config.php
+++ _config.php
@@ -3,32 +3,21 @@
 define('SHOP_DIR',basename(__DIR__));
 define('SHOP_PATH',BASE_PATH.DIRECTORY_SEPARATOR.SHOP_DIR);
 
-// Extend the Member with e-commerce related fields.
-DataObject::add_extension('Member', 'ShopMember');
-// Extend Payment with e-commerce relationship.
-if(!class_exists('Payment')) user_error("You need to also install the Payment module to use the shop module", E_USER_ERROR);
-DataObject::add_extension('Payment', 'ShopPayment');
-//create controller for shopping cart
+if(!class_exists('Payment'))
+	user_error("You need to also install the Payment module to use the shop module", E_USER_ERROR);
 
-Object::add_extension("Page_Controller","ViewableCart");
-Object::add_extension("ShoppingCart_Controller","ViewableCart");
-Object::add_extension("OrderAttribute","OrderAttributeAJAX");
-Object::add_extension("Order","OrderAJAX");
-Object::add_extension("HasManyList","OrderItemList");
-Object::add_extension("SiteConfig", "ShopConfig");
+//extensions
+SiteConfig::add_extension("ShopConfig");
+Payment::add_extension("ShopPayment");
+Page_Controller::add_extension("ViewableCart");
+ShoppingCart_Controller::add_extension("ViewableCart");
+OrderAttribute::add_extension("OrderAttributeAJAX");
+Order::add_extension("OrderAJAX");
+HasManyList::add_extension("OrderItemList");
+Member::add_extension('ShopMember');
+//variations
+Product::add_extension("ProductVariationDecorator");
+Product_Controller::add_extension("ProductControllerVariationExtension");
 
 //custom classes
 Object::useCustomClass('Currency','ShopCurrency', true);
-//Object::useCustomClass('Versioned','FixVersioned');
-
-//variations
-Object::add_extension("Product","ProductVariationDecorator");
-Object::add_extension("Product_Controller","ProductControllerVariationExtension");
-
-//reports
-/*
-SS_Report::register("SideReport", "ShopSideReport_AllProducts");
-SS_Report::register("SideReport", "ShopSideReport_FeaturedProducts");
-SS_Report::register("SideReport", "ShopSideReport_NoImageProducts");
-SS_Report::register("SideReport", "ShopSideReport_HeavyProducts");
-*/
