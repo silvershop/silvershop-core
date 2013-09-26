5a1d0405c7c3ad15a1d58f275f350205ce6f0acb
Paul Bennett <paul@moveforward.co.nz>
2013-09-27 10:45:31 +1200

    closes #138 - new unit test confirms that CheckoutPage::find_link() returns correct link path for nested checkout pages

---
+++ tests/forms/NestedCheckoutTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class NestedCheckoutTest extends SapphireTest{
+	
+	static $fixture_file = 'shop/tests/fixtures/pages/NestedCheckout.yml';
+	
+	function setUp(){
+		parent::setUp();
+		$this->checkoutpage = $this->objFromFixture('CheckoutPage', 'checkout');
+	}
+	
+	function testNestedCheckoutForm(){
+		
+		$this->assertEquals('/shop/checkout/', CheckoutPage::find_link(), 'Link is: ' . CheckoutPage::find_link());
+	}
+	
+}
