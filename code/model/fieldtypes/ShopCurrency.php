8d8dfc936a1db64310a07644a8aa6c7ecf6e4904
Jeremy Shipman <jeremy@burnbright.net>
2013-12-09 15:15:39 +1300

    Removed unneeded function from ShopCurrency.php

--- code/model/fieldtypes/ShopCurrency.php
+++ code/model/fieldtypes/ShopCurrency.php
@@ -8,12 +8,8 @@ class ShopCurrency extends Currency {
 	protected static $thousand_delimiter = ',';
 	protected static $negative_value_format = "<span class=\"negative\">(%s)</span>";
 
-	public static function getCurrencySymbol() {
-		return Currency::config()->currency_symbol;
-	}
-
 	function Nice() {
-		$val = self::getCurrencySymbol() . number_format(abs($this->value), 2, self::getDecimalDelimiter(), self::getThousandDelimiter());
+		$val = $this->config()->currency_symbol . number_format(abs($this->value), 2, self::getDecimalDelimiter(), self::getThousandDelimiter());
 		if($this->value < 0){
 			return sprintf(self::$negative_value_format,$val);
 		}
