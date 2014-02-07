91855985a0aef8981bf900ef5be11d63800752e5
Jeremy Shipman <jeremy@burnbright.net>
2014-02-07 13:34:34 +1300

    Improved AccountPage test to run default address / create address code.

--- tests/AccountPageTest.php
+++ tests/AccountPageTest.php
@@ -1,23 +1,49 @@
 <?php
 
-class AccountPageTest extends SapphireTest{
+class AccountPageTest extends FunctionalTest{
 
 	public static $fixture_file = "shop/tests/fixtures/shop.yml";
+	public static $disable_theme = true;
+	public static $use_draft_site = true;
 
-	public function setUp(){
+	public function setUp() {
 		parent::setUp();
-		$this->controller = new AccountPage_Controller($this->objFromFixture("AccountPage", "accountpage"));
+		$this->accountpage = $this->objFromFixture("AccountPage", "accountpage");
+		$this->controller = new AccountPage_Controller($this->accountpage);
 		$this->controller->init();
 	}
 
-	public function testAddressBook(){
-		$this->controller->addressbook();
-
-		//TODO: save address
-		//TODO: save default
+	public function testGlobals() {
+		$this->assertFalse($this->accountpage->canCreate(), "account page exists");
+		$this->assertEquals(Director::baseURL()."account/", AccountPage::find_link());
+		$this->assertEquals(Director::baseURL()."account/order/10", AccountPage::get_order_link(10));
 	}
 
-	public function testEditProfile(){
+	public function testAddressBook() {
+
+		$member = $this->objFromFixture("Member", "joebloggs");
+		$this->logInAs($member);
+
+		$this->controller->init(); //reinit to connect up member
+
+		$address = $this->objFromFixture("Address", "foobar");
+		$address->MemberID = $member->ID;
+		$address->write();
+
+		$forms = $this->controller->addressbook();
+		$createform = $forms['CreateAddressForm'];
+		$defaultform = $forms['DefaultAddressForm'];
+
+		$this->assertTrue($member->AddressBook()->exists());
+
+		$this->assertTrue((boolean)$createform, "Create form exists");
+		$this->assertTrue((boolean)$defaultform, "Default form exists");
+
+		//$this->controller->saveaddresses($data, $createform); //TODO: save address
+		//$this->controller->savedefaultaddresses($data, $defaultform); //TODO: save default
+	}
+
+	public function testEditProfile() {
 		$this->controller->editprofile();
 		$this->controller->EditAccountForm();
 		$this->controller->ChangePasswordForm();
