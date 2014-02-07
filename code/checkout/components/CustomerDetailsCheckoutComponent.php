eb68a078291299716dee3f78dfaae3af360d09eb
Jeremy Shipman <jeremy@burnbright.net>
2014-02-07 13:31:13 +1300

    Don't generate the contact details step form if there is no order available. Also, fill out customer details from current member object, if not already present in order.

--- code/checkout/components/CustomerDetailsCheckoutComponent.php
+++ code/checkout/components/CustomerDetailsCheckoutComponent.php
@@ -6,35 +6,39 @@ class CustomerDetailsCheckoutComponent extends CheckoutComponent{
 		'FirstName','Surname','Email'
 	);
 
-	public function getFormFields(Order $order){
+	public function getFormFields(Order $order) {
 		$fields = new FieldList(
-			$firstname = TextField::create('FirstName', _t('CheckoutField.FIRSTNAME','First Name')),
-			$surname = TextField::create('Surname', _t('CheckoutField.SURNAME','Surname')),
-			$email = EmailField::create('Email', _t('CheckoutField.EMAIL','Email'))
+			$firstname = TextField::create('FirstName', _t('CheckoutField.FIRSTNAME', 'First Name')),
+			$surname = TextField::create('Surname', _t('CheckoutField.SURNAME', 'Surname')),
+			$email = EmailField::create('Email', _t('CheckoutField.EMAIL', 'Email'))
 		);
-		//populate fields with member details, if logged in
-		if($member = Member::currentUser()){
-			$firstname->setValue($member->FirstName);
-			$surname->setValue($member->Surname);
-			$email->setValue($member->Email);
-		}
 
 		return $fields;
 	}
 
-	public function validateData(Order $order, array $data){
+	public function validateData(Order $order, array $data) {
 		//all fields are required
 	}
 
-	public function getData(Order $order){
-		return array(
-			'FirstName' => $order->FirstName,
-			'Surname' => $order->Surname,
-			'Email' => $order->Email
-		);
+	public function getData(Order $order) {
+		if($order->FirstName || $order->Surname || $order->Email){
+			return array(
+				'FirstName' => $order->FirstName,
+				'Surname' => $order->Surname,
+				'Email' => $order->Email
+			);
+		}
+		if($member = Member::currentUser()){
+			return array(
+				'FirstName' => $member->FirstName,
+				'Surname' => $member->Surname,
+				'Email' => $member->Email
+			);
+		}
+		return array();
 	}
 
-	public function setData(Order $order, array $data){
+	public function setData(Order $order, array $data) {
 		$order->update($data);
 		$order->write();
 	}
--- code/checkout/steps/CheckoutStep_ContactDetails.php
+++ code/checkout/steps/CheckoutStep_ContactDetails.php
@@ -7,31 +7,27 @@ class CheckoutStep_ContactDetails extends CheckoutStep{
 		'ContactDetailsForm'
 	);
 
-	public function contactdetails(){
+	public function contactdetails() {
 		return array(
 			'OrderForm' => $this->ContactDetailsForm()
 		);
 	}
 
-	public function ContactDetailsForm(){
-		$form = new CheckoutForm($this->owner, 'ContactDetailsForm', $this->checkoutconfig());
-		$form->setActions(new FieldList(
-			new FormAction("setcontactdetails","Continue")
-		));
-		$this->owner->extend('updateContactDetailsForm',$form);
-
-		return $form;
-	}
-
-	public function setcontactdetails($data,$form){
-		$this->checkoutconfig()->setData($form->getData());
-		$this->owner->redirect($this->NextStepLink());
-	}
-
-	public function checkoutconfig(){
+	public function ContactDetailsForm() {
+		$cart = ShoppingCart::curr();
+		if(!$cart){
+			return false;
+		}
 		$config = new CheckoutComponentConfig(ShoppingCart::curr());
 		$config->addComponent(new CustomerDetailsCheckoutComponent());
-		return $config;
+		$form = new CheckoutForm($this->owner, 'ContactDetailsForm', $config);
+		$form->setRedirectLink($this->NextStepLink());
+		$form->setActions(new FieldList(
+			new FormAction("checkoutSubmit", "Continue")
+		));
+		$this->owner->extend('updateContactDetailsForm', $form);
+
+		return $form;
 	}
 
 }
