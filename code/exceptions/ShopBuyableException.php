2177dbe8984eb739ab94962551e593852912781b
Will Rossiter <will.rossiter@gmail.com>
2014-03-09 21:12:28 +1300

    Add exceptions

---
+++ code/exceptions/ShopBuyableException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package shop
+ * @subpackage exceptions
+ */
+class ShopBuyableException extends Exception {
+	
+}
+
+/**
+ * @package shop
+ * @subpackage exceptions
+ */
+class ShopBuyableException_NotEnoughStock extends ShopBuyableException {
+	
+}
