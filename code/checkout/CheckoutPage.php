1b5459cba20674c3915ea5361829b8ef8f2e87d2
Ingo Schommer <ingo@silverstripe.com>
2014-03-18 12:16:53 +1300

    Extension points for PaymentForm

--- code/checkout/CheckoutPage.php
+++ code/checkout/CheckoutPage.php
@@ -91,8 +91,8 @@ class CheckoutPage_Controller extends Page_Controller {
 			'OrderForm',
 			Injector::inst()->create("CheckoutComponentConfig", ShoppingCart::curr())
 		);
-
 		$form->Cart = $this->Cart();
+		$this->extend('updateOrderForm', $form);
 
 		return $form;
 	}
@@ -120,12 +120,11 @@ class CheckoutPage_Controller extends Page_Controller {
 		$config->AddComponent(new OnsitePaymentCheckoutComponent());
 
 		$form = PaymentForm::create($this, "PaymentForm", $config);
-
 		$form->setActions(new FieldList(
 			FormAction::create("submitpayment", "Submit Payment")
 		));
-
 		$form->setFailureLink($this->Link());
+		$this->extend('updatePaymentForm', $form);
 
 		return $form;
 	}
