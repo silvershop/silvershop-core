56b6cae478ff59a4d519b7e7c36ee8453122dddd
Jeremy Shipman <jeremy@burnbright.net>
2014-04-29 11:46:07 +1200

    Added another cart to shop.yml fixture Converted ShopTest to a helper class.

--- tests/ShopTest.php
+++ tests/ShopTest.php
@@ -1,19 +1,30 @@
 <?php
 /**
- * High level tests of the whole ecommerce module.
+ * Helper class for setting up shop tests
  *
  * @package shop
  * @subpackage tests
  */
-class ShopTest extends SapphireTest {
-
-	public function testExampleConfig() {
-		$this->markTestIncomplete('get example from yaml');
-	}
+class ShopTest{
 
 	public static function setConfiguration() {
 		$ds = DIRECTORY_SEPARATOR;
 		include BASE_PATH.$ds.SHOP_DIR.$ds.'tests'.$ds.'test_config.php';
+	}
+
+	/**
+	 * Helper function for publishing products,
+	 * since the fixture system doesn't do it for us.
+	 *
+	 * Note: the shop.yml fixture MUST be included in the test class fixtures list
+	 */
+	public static function publishProducts(SapphireTest $test){
+		$test->objFromFixture("Product", "socks")
+			->publish("Stage", "Live");
+		$test->objFromFixture("Product", "tshirt")
+			->publish("Stage", "Live");
+		$test->objFromFixture("Product", "mp3player")
+			->publish("Stage", "Live");
 	}
 
 }
