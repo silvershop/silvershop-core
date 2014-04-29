fe4cab6c26787f23966860187a6528f3d9822704
Jeremy Shipman <jeremy@burnbright.net>
2014-04-30 09:46:05 +1200

    Removed publishProducts function from ShopTest, as it doesn't work.

--- tests/ShopTest.php
+++ tests/ShopTest.php
@@ -12,19 +12,4 @@ class ShopTest{
 		include BASE_PATH.$ds.SHOP_DIR.$ds.'tests'.$ds.'test_config.php';
 	}
 
-	/**
-	 * Helper function for publishing products,
-	 * since the fixture system doesn't do it for us.
-	 *
-	 * Note: the shop.yml fixture MUST be included in the test class fixtures list
-	 */
-	public static function publishProducts(SapphireTest $test){
-		$test->objFromFixture("Product", "socks")
-			->publish("Stage", "Live");
-		$test->objFromFixture("Product", "tshirt")
-			->publish("Stage", "Live");
-		$test->objFromFixture("Product", "mp3player")
-			->publish("Stage", "Live");
-	}
-
 }
