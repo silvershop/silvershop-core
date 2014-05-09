7f7536c718a482c14b11c64fa3298d6a1908b775
Jeremy Shipman <jeremy@burnbright.net>
2014-05-09 16:03:22 +1200

    Minor tidy of CheckoutForm

--- code/checkout/CheckoutForm.php
+++ code/checkout/CheckoutForm.php
@@ -9,28 +9,22 @@ class CheckoutForm extends Form {
 	public function __construct($controller, $name, CheckoutComponentConfig $config) {
 		$this->config = $config;
 		$fields = $config->getFormFields();
-
 		$actions = new FieldList(
 			FormAction::create(
 				'checkoutSubmit',
 				_t('CheckoutForm', 'Proceed to payment')
 			)
-		);
-
+	);
 		$validator = new CheckoutComponentValidator($this->config);
-
 		parent::__construct($controller, $name, $fields, $actions, $validator);
-
+		//load data from various sources
 		$this->loadDataFrom($this->config->getData(), Form::MERGE_IGNORE_FALSEISH);
-
 		if($member = Member::currentUser()) {
 			$this->loadDataFrom($member, Form::MERGE_IGNORE_FALSEISH);
 		}
-
 		if($sessiondata = Session::get("FormInfo.{$this->FormName()}.data")){
 			$this->loadDataFrom($sessiondata, Form::MERGE_IGNORE_FALSEISH);
 		}
-
 	}
 
 	public function setRedirectLink($link) {
@@ -40,8 +34,8 @@ class CheckoutForm extends Form {
 	public function checkoutSubmit($data, $form) {
 		//form validation has passed by this point, so we can save data
 		$this->config->setData($form->getData());
-
 		if($this->redirectlink) {
+
 			return $this->controller->redirect($this->redirectlink);
 		}
 
