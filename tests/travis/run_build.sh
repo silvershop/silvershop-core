31176206fe42d7de4a492518808c63ec0c8e6bc1
Mark Guinn <mark@adaircreative.com>
2015-04-18 16:31:54 +0100

    coverage on scrutinizer

--- tests/travis/run_build.sh
+++ tests/travis/run_build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 if [ "$COVERAGE" = "1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
 	vendor/bin/phpunit -c shop/phpunit.xml.dist --coverage-clover shop/coverage.xml
-#elif [ "$COVERAGE" = "2" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-#	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split2 --coverage-clover shop/coverage.xml
-#elif [ "$COVERAGE" = "3" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-#	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split3 --coverage-clover shop/coverage.xml
+elif [ "$COVERAGE" = "2" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split2 --coverage-clover shop/coverage.xml
+elif [ "$COVERAGE" = "3" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split3 --coverage-clover shop/coverage.xml
 else
 	vendor/bin/phpunit -c shop/phpunit.xml.dist
 fi
