78b9cf58419749bd493ac8d0c148ae1ca2c4632d
Mark Guinn <mark@adaircreative.com>
2015-10-29 08:24:13 +0000

    Trying to fix scrutinizer again

--- tests/travis/upload_coverage.sh
+++ tests/travis/upload_coverage.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-echo "coverage = $COVERAGE, slug = $TRAVIS_REPO_SLUG, commit=$TRAVIS_COMMIT"
+echo "coverage = $COVERAGE, slug = $TRAVIS_REPO_SLUG, commit = $TRAVIS_COMMIT"
 if [ -n "$COVERAGE" ]; then
+	rm -rf .git
 	cd shop
 	wget https://scrutinizer-ci.com/ocular.phar
-	php ocular.phar code-coverage:upload -v --format=php-clover --repository=g/$TRAVIS_REPO_SLUG --revision=$TRAVIS_COMMIT ~/builds/ss/shop/coverage.xml
+	php ocular.phar code-coverage:upload -v --format=php-clover ~/builds/ss/shop/coverage.xml
 fi
