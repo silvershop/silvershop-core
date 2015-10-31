747a4e4f95a48758fcdd8e819046f7638331e0aa
Mark Guinn <mark@adaircreative.com>
2015-10-31 09:39:10 +0000

    Added some debugging output to coverage upload 4

--- tests/travis/upload_coverage.sh
+++ tests/travis/upload_coverage.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
-echo "coverage = $COVERAGE, slug = $TRAVIS_REPO_SLUG, commit = $TRAVIS_COMMIT"
+echo "coverage = $COVERAGE, slug = $TRAVIS_REPO_SLUG, commit=$TRAVIS_COMMIT"
 if [ -n "$COVERAGE" ]; then
-#	rm -rf .git
-	cd ~/builds/ss/shop
+	cd shop
 	wget https://scrutinizer-ci.com/ocular.phar
-	pwd
-	ls -al ~
-	ls -al ~/builds
-	ls -al ~/builds/ss
-	ls -al ~/builds/ss/shop
-	php ocular.phar code-coverage:upload -v --format=php-clover ~/builds/ss/shop/coverage.xml
+	php ocular.phar code-coverage:upload -v --format=php-clover --repository=g/$TRAVIS_REPO_SLUG --revision=$TRAVIS_COMMIT ~/builds/ss/shop/coverage.xml
 fi
