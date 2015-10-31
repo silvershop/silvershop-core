32bad292f388660776d2c76d93aa8f10c8e30875
Mark Guinn <mark@adaircreative.com>
2015-10-31 09:45:13 +0000

    trying something to preserve .git

--- tests/travis/upload_coverage.sh
+++ tests/travis/upload_coverage.sh
@@ -2,6 +2,7 @@
 echo "coverage = $COVERAGE, slug = $TRAVIS_REPO_SLUG, commit=$TRAVIS_COMMIT"
 if [ -n "$COVERAGE" ]; then
 	cd shop
+	tar xf ~/gitbackup.tar
 	wget https://scrutinizer-ci.com/ocular.phar
 	php ocular.phar code-coverage:upload -v --format=php-clover --repository=g/$TRAVIS_REPO_SLUG --revision=$TRAVIS_COMMIT ~/builds/ss/shop/coverage.xml
 fi
