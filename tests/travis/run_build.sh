00b336b02cc5082a813410de49513126644aaeb0
Mark Guinn <mark@adaircreative.com>
2016-02-23 22:32:19 +0000

    Update run_build.sh

--- tests/travis/run_build.sh
+++ tests/travis/run_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #if [ "$COVERAGE" = "1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
 if [ -n "$COVERAGE" ]; then
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split$COVERAGE --coverage-clover ~/builds/ss/shop/coverage.xml
+	vendor/bin/phpunit -c ~/builds/ss/silvershop/phpunit.xml.dist --testsuite Split$COVERAGE --coverage-clover ~/builds/ss/silvershop/coverage.xml
 else
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist
+	vendor/bin/phpunit -c ~/builds/ss/silvershop/phpunit.xml.dist
 fi
