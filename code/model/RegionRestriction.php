9b426ddf994c4b95b5d9ba18e50c4a873c3231d6
Stephen Honor <stephen@daykinandstorey.co.uk>
2016-02-26 12:55:30 +0000

    Added partial postcode support for postal codes

--- code/model/RegionRestriction.php
+++ code/model/RegionRestriction.php
@@ -42,6 +42,27 @@ class RegionRestriction extends DataObject
         'PostalCode' => 'TextField',
     );
 
+
+    public static function parse_uk_postcode($postcode) {
+        $postcode = str_replace(' ', '', $postcode); // remove any spaces;
+        $postcode = strtoupper($postcode); // force to uppercase;
+        $valid_postcode_exp = "/^(([A-PR-UW-Z]{1}[A-IK-Y]?)([0-9]?[A-HJKS-UW]?[ABEHMNPRVWXY]?|[0-9]?[0-9]?))\s?([0-9]{1}[ABD-HJLNP-UW-Z]{2})$/i";
+
+        // set default output results (assuming invalid postcode):
+        $output['validate'] = false;
+        $output['prefix'] = '';
+        $output['suffix'] = '';
+
+        if (preg_match($valid_postcode_exp, strtoupper($postcode))) {
+            $output['validate'] = true;
+            $suffix = substr($postcode, -3);
+            $prefix = str_replace($suffix, '', $postcode);
+            $output['prefix'] = $prefix;
+            $output['suffix'] = $suffix;
+        }
+        return $output;
+    }
+
     /**
      * Produce a SQL filter to get matching RegionRestrictions to a given address
      *
@@ -53,7 +74,6 @@ class RegionRestriction extends DataObject
             "Country",
             "State",
             "City",
-            "PostalCode",
         );
         $where = array();
         $rr = "\"RegionRestriction\".";
@@ -61,6 +81,22 @@ class RegionRestriction extends DataObject
             $where[] = "TRIM(LOWER($rr\"$field\")) = TRIM(LOWER('" . Convert::raw2sql($address->$field)
                 . "')) OR $rr\"$field\" = '*' OR $rr\"$field\" = ''";
         }
+
+        // more advanced uk postcode checks
+        // will check for partial postcodes (eg. NE, NE17, NE177AH)
+        $postcode = self::parse_uk_postcode($address->PostalCode);
+        if (isset($postcode['validate']) && $postcode['validate']) {
+            $region = preg_replace("/[^a-z]+/i", "", substr($postcode['prefix'],0,2));
+            $where[] = "TRIM(LOWER($rr\"PostalCode\")) = TRIM(LOWER('" . Convert::raw2sql($region)
+                . "')) OR TRIM(LOWER($rr\"PostalCode\")) = TRIM(LOWER('" . Convert::raw2sql($postcode['prefix'])
+                . "')) OR TRIM(LOWER($rr\"PostalCode\")) = TRIM(LOWER('" . Convert::raw2sql($postcode['prefix'].$postcode['suffix'])
+                . "')) OR $rr\"PostalCode\" = '*' OR $rr\"PostalCode\" = ''";
+        }
+        else {
+            $where[] = "TRIM(LOWER($rr\"PostalCode\")) = TRIM(LOWER('" . Convert::raw2sql($address->PostalCode)
+                . "')) OR $rr\"PostalCode\" = '*' OR $rr\"PostalCode\" = ''";
+        }
+
         return "(" . implode(") AND (", $where) . ")";
     }
 
