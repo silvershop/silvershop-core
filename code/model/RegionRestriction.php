9c6def41b3aa23411088191b9332dee9512c84f2
Stephen Honor <stephen@daykinandstorey.co.uk>
2016-03-02 14:49:47 +0000

    Added support for partial postcodes within GB

--- code/model/RegionRestriction.php
+++ code/model/RegionRestriction.php
@@ -43,6 +43,32 @@ class RegionRestriction extends DataObject
     );
 
     /**
+     * Parses a UK postcode to give you the different sections
+     *
+     * @param str $postcode
+     * @return array
+     */
+    public static function parse_uk_postcode($postcode) {
+        $postcode = str_replace(' ', '', $postcode); // remove any spaces;
+        $postcode = strtoupper($postcode); // force to uppercase;
+        $valid_postcode_exp = "/^(([A-PR-UW-Z]{1}[A-IK-Y]?)([0-9]?[A-HJKS-UW]?[ABEHMNPRVWXY]?|[0-9]?[0-9]?))\s?([0-9]{1}[ABD-HJLNP-UW-Z]{2})$/i";
+
+        // set default output results (assuming invalid postcode):
+        $output['validate'] = false;
+        $output['prefix'] = '';
+        $output['suffix'] = '';
+
+        if (preg_match($valid_postcode_exp, strtoupper($postcode))) {
+            $output['validate'] = true;
+            $suffix = substr($postcode, -3);
+            $prefix = str_replace($suffix, '', $postcode);
+            $output['prefix'] = $prefix;
+            $output['suffix'] = $suffix;
+        }
+        return $output;
+    }
+
+    /**
      * Produce a SQL filter to get matching RegionRestrictions to a given address
      *
      * @param Address $address
@@ -52,8 +78,7 @@ class RegionRestriction extends DataObject
         $restrictables = array(
             "Country",
             "State",
-            "City",
-            "PostalCode",
+            "City"
         );
         $where = array();
         $rr = "\"RegionRestriction\".";
@@ -61,6 +86,28 @@ class RegionRestriction extends DataObject
             $where[] = "TRIM(LOWER($rr\"$field\")) = TRIM(LOWER('" . Convert::raw2sql($address->$field)
                 . "')) OR $rr\"$field\" = '*' OR $rr\"$field\" = ''";
         }
+
+        // check that the country is set to the uk
+        if($address->Country == 'GB') {
+            // will check for partial postcodes (eg. NE, NE17, NE177AH)
+            $postcode = self::parse_uk_postcode($address->PostalCode);
+            if (isset($postcode['validate']) && $postcode['validate']) {
+                $region = preg_replace("/[^a-z]+/i", "", substr($postcode['prefix'],0,2));
+                $where[] = "TRIM(LOWER($rr\"PostalCode\")) = TRIM(LOWER('" . Convert::raw2sql($region)
+                    . "')) OR TRIM(LOWER($rr\"PostalCode\")) = TRIM(LOWER('" . Convert::raw2sql($postcode['prefix'])
+                    . "')) OR TRIM(LOWER($rr\"PostalCode\")) = TRIM(LOWER('" . Convert::raw2sql($postcode['prefix'].$postcode['suffix'])
+                    . "')) OR $rr\"PostalCode\" = '*' OR $rr\"PostalCode\" = ''";
+            }
+            else {
+                $where[] = "TRIM(LOWER($rr\"PostalCode\")) = TRIM(LOWER('" . Convert::raw2sql($address->PostalCode)
+                    . "')) OR $rr\"PostalCode\" = '*' OR $rr\"PostalCode\" = ''";
+            }
+        }
+        else {
+            $where[] = "TRIM(LOWER($rr\"PostalCode\")) = TRIM(LOWER('" . Convert::raw2sql($address->PostalCode)
+                . "')) OR $rr\"PostalCode\" = '*' OR $rr\"PostalCode\" = ''";
+        }
+
         return "(" . implode(") AND (", $where) . ")";
     }
 
