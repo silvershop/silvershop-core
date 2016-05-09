95e34b1f2a8daa363aba376ff91dbb97a51d5d60
Mark Guinn <mark@adaircreative.com>
2016-05-09 16:49:22 -0500

    Added logic to prevent upscaling product images

--- code/product/ProductImage.php
+++ code/product/ProductImage.php
@@ -7,39 +7,73 @@
  */
 class Product_Image extends DataExtension
 {
-    public function getThumbnail()
+    /**
+     * @param bool $upscale [optional]
+     * @return Image
+     */
+    public function getThumbnail($upscale = false)
     {
         $width = self::config()->thumbnail_width;
         $height = self::config()->thumbnail_height;
 
-        return $this->getImageAt($width, $height);
+        return $this->getImageAt($width, $height, $upscale);
     }
 
-    public function getContentImage()
+    /**
+     * @param bool $upscale [optional]
+     * @return Image
+     */
+    public function getContentImage($upscale = false)
     {
         $width = self::config()->content_image_width;
         $height = self::config()->content_image_height;
 
-        return $this->getImageAt($width, $height);
+        return $this->getImageAt($width, $height, $upscale);
     }
 
-    public function getLargeImage()
+    /**
+     * @param bool $upscale [optional]
+     * @return Image
+     */
+    public function getLargeImage($upscale = false)
     {
         $width = self::config()->large_image_width;
         $height = self::config()->large_image_height;
 
-        return $this->getImageAt($width, $height);
+        return $this->getImageAt($width, $height, $upscale);
     }
 
-    public function getImageAt($width = null, $height = null)
+    /**
+     * Resizes image by width or height only if the source image is bigger than the given width/height.
+     * This prevents ugly upscaling.
+     *
+     * @param int  $width [optional]
+     * @param int  $height [optional]
+     * @param bool $upscale [optional]
+     *
+     * @return Image
+     */
+    public function getImageAt($width = null, $height = null, $upscale = false)
     {
+        if (!$this->owner->exists()) {
+            return $this->owner;
+        }
+
+        $dim = explode('x', $this->owner->getDimensions());
+
         if ($width && $height) {
-            return $this->owner->SetSize($width, $height);
+            return $dim[0] < $width && $dim[1] < $height && !$upscale
+                ? $this->owner
+                : $this->owner->SetSize($width, $height);
         } else {
             if ($width) {
-                return $this->owner->SetWidth($width);
+                return $dim[0] < $width && !$upscale
+                    ? $this->owner
+                    : $this->owner->SetWidth($width);
             } else {
-                return $this->owner->SetHeight($height);
+                return $dim[1] < $width && !$upscale
+                    ? $this->owner
+                    : $this->owner->SetHeight($height);
             }
         }
     }
