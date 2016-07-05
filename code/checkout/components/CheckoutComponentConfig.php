0bd046167ca76eae2724f87785d66aee416b56fe
wildflower <wildflower1975@gmail.com>
2016-07-05 13:09:53 +0800

    Optional Shipping
    I've been working with subscription products and so don't need the
    shipping address form and the validation that comes with it.
    
    I've added this switch in the Checkout Config to turn the shipping
    address off:
    CheckoutConfig:
    ShippingOptional: true

--- code/checkout/components/CheckoutComponentConfig.php
+++ code/checkout/components/CheckoutComponentConfig.php
@@ -217,7 +217,9 @@ class SinglePageCheckoutComponentConfig extends CheckoutComponentConfig
     {
         parent::__construct($order);
         $this->addComponent(CustomerDetailsCheckoutComponent::create());
-        $this->addComponent(ShippingAddressCheckoutComponent::create());
+         if(!CheckoutConfig::config()->ShippingOptional){ 
+			$this->addComponent(ShippingAddressCheckoutComponent::create());
+		}
         $this->addComponent(BillingAddressCheckoutComponent::create());
         if (Checkout::member_creation_enabled() && !Member::currentUserID()) {
             $this->addComponent(MembershipCheckoutComponent::create());
