9e8d4b87f4cb2305d65be5f2c9536dd3c8576fee
Roman Schmid <bummzack@gmail.com>
2018-02-20 10:54:31 +0100

    Fix escaping of HTML in terms and conditions checkbox label.

--- src/Checkout/Component/Terms.php
+++ src/Checkout/Component/Terms.php
@@ -5,6 +5,7 @@ namespace SilverShop\Checkout\Component;
 use SilverShop\Model\Order;
 use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\FieldType\DBField;
 use SilverStripe\SiteConfig\SiteConfig;
 
 class Terms extends CheckoutComponent
@@ -18,12 +19,12 @@ class Terms extends CheckoutComponent
             $fields->push(
                 CheckboxField::create(
                     'ReadTermsAndConditions',
-                    _t(
+                    DBField::create_field('HTMLText', _t(
                         'SilverShop\Checkout\Checkout.TermsAndConditionsLink',
                         'I agree to the terms and conditions stated on the <a href="{TermsPageLink}" target="new" title="Read the shop terms and conditions for this site">{TermsPageTitle}</a> page',
                         '',
                         ['TermsPageLink' => $page->Link(), 'TermsPageTitle' => $page->Title]
-                    )
+                    ))
                 )->setCustomValidationMessage(
                     _t('SilverShop\Checkout\CheckoutField.MustAgreeToTerms', 'You must agree to the terms and conditions')
                 )
