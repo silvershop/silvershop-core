0fa6e733ca99102d625715a00af3ac2228629c26
Florian Thoma <florian.thoma@innoweb.com.au>
2018-12-04 20:24:35 +1100

    fix use statements

--- src/Admin/OrdersAdmin.php
+++ src/Admin/OrdersAdmin.php
@@ -7,10 +7,9 @@ use SilverShop\Model\Order;
 use SilverShop\Model\OrderStatusLog;
 use SilverStripe\Admin\ModelAdmin;
 use SilverStripe\Forms\GridField\GridFieldAddNewButton;
-use SilverStripe\Forms\GridField\GridFieldConfig;
-use SilverStripe\Forms\GridField\GridFieldDataColumns;
 use SilverStripe\Forms\GridField\GridFieldDetailForm;
 use SilverStripe\Forms\GridField\GridFieldSortableHeader;
+use SilverStripe\ORM\DataObject;
 
 /**
  * Order administration interface, based on ModelAdmin
@@ -19,64 +18,62 @@ use SilverStripe\Forms\GridField\GridFieldSortableHeader;
  */
 class OrdersAdmin extends ModelAdmin
 {
-    private static $url_segment = 'orders';
+	private static $url_segment = 'orders';
 
-    private static $menu_title = 'Orders';
+	private static $menu_title = 'Orders';
 
-    private static $menu_priority = 1;
+	private static $menu_priority = 1;
 
-    private static $menu_icon_class = 'silvershop-icon-cart';
+	private static $menu_icon_class = 'silvershop-icon-cart';
 
-    private static $managed_models = [
-        Order::class,
-        OrderStatusLog::class
-    ];
+	private static $managed_models = [
+		Order::class,
+		OrderStatusLog::class
+	];
 
-    private static $model_importers = array();
+	private static $model_importers = array();
 
-    /**
-     * Restrict list to non-hidden statuses
-     */
-    public function getList()
-    {
-        if ($this->modelClass == Order::class) {
-			
+	/**
+	 * Restrict list to non-hidden statuses
+	 */
+	public function getList()
+	{
+		if ($this->modelClass == Order::class) {
 			$list = DataObject::singleton($this->modelClass)->get()
 				->exclude('Status', Order::config()->hidden_status); //exclude hidden statuses;
 
-            $this->extend('updateList', $list);
-			
-            return $list;
-        } else {
-            return parent::getList();
-        }
-    }
+			$this->extend('updateList', $list);
+			return $list;
+		} else {
+			return parent::getList();
+		}
+	}
 
-    /**
-     * Replace gridfield detail form to include print functionality
-     */
-    function getEditForm($id = null, $fields = null)
-    {
-        $form = parent::getEditForm($id, $fields);
-        if ($this->modelClass == $this->sanitiseClassName(Order::class)) {
-            /** @var GridFieldConfig $config */
-            $config = $form->Fields()->fieldByName($this->modelClass)->getConfig();
-            $config->getComponentByType(GridFieldSortableHeader::class)->setFieldSorting(
-                [
-                    'StatusI18N' => 'Status'
-                ]
-            );
-            $config
-                ->getComponentByType(GridFieldDetailForm::class)
-                ->setItemRequestClass(OrderGridFieldDetailForm_ItemRequest::class); //see below
-        }
-        if ($this->modelClass == $this->sanitiseClassName(OrderStatusLog::class)) {
-            /** @var GridFieldConfig $config */
-            $config = $form->Fields()->fieldByName($this->modelClass)->getConfig();
-            // Remove add new button
-            $config->removeComponentsByType($config->getComponentByType(GridFieldAddNewButton::class));
-        }
+	/**
+	 * Replace gridfield detail form to include print functionality
+	 */
+	function getEditForm($id = null, $fields = null)
+	{
+		$form = parent::getEditForm($id, $fields);
+		if ($this->modelClass == $this->sanitiseClassName(Order::class)) {
+			/** @var GridFieldConfig $config */
+			$config = $form->Fields()->fieldByName($this->modelClass)->getConfig();
+			$config->getComponentByType(GridFieldSortableHeader::class)->setFieldSorting(
+				[
+					'StatusI18N' => 'Status'
+				]
+			);
+			$config
+				->getComponentByType(GridFieldDetailForm::class)
+				->setItemRequestClass(OrderGridFieldDetailForm_ItemRequest::class); //see below
+		}
+		if ($this->modelClass == $this->sanitiseClassName(OrderStatusLog::class)) {
+			/** @var GridFieldConfig $config */
+			$config = $form->Fields()->fieldByName($this->modelClass)->getConfig();
+			// Remove add new button
+			$config->removeComponentsByType($config->getComponentByType(GridFieldAddNewButton::class));
+		}
 
-        return $form;
-    }
+		return $form;
+	}
 }
