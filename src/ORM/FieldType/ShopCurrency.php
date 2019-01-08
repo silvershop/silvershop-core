475b8570acb582b3d994ac27b6d56f82f36a9408
Werner M. Krauß <werner.krauss@netwerkstatt.at>
2019-01-08 20:14:45 +0100

    casting of ShopCurrency

--- src/ORM/FieldType/ShopCurrency.php
+++ src/ORM/FieldType/ShopCurrency.php
@@ -43,6 +43,12 @@ class ShopCurrency extends DBCurrency
      */
     private static $negative_value_format = '<span class="negative">(%s)</span>';
 
+    private static $casting = [
+        'forTemplate' => 'HTMLFragment',
+        'Nice' => 'HTMLFragment',
+        'NiceOrEmpty' => 'HTMLFragment',
+    ];
+
     public function Nice()
     {
         $symbol = $this->config()->currency_symbol;
