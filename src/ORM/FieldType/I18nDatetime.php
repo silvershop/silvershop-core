0edfd9df2b7c6a02b1a503ede0e62e8e1abf956a
Will Rossiter <will@fullscreen.io>
2019-01-13 14:45:42 +1300

    Don’t use owner API as class is setup as subclass rather than an extension.

--- src/ORM/FieldType/I18nDatetime.php
+++ src/ORM/FieldType/I18nDatetime.php
@@ -5,34 +5,44 @@ namespace SilverShop\ORM\FieldType;
 use SilverStripe\ORM\FieldType\DBDatetime;
 
 /**
- * provides I18n formating
- *
- * @package    shop
- * @subpackage fieldtype
- * @property   DBDatetime $owner
+ * Provides i18n formatting
  */
 class I18nDatetime extends DBDatetime
 {
     /**
      * Returns the datetime in the format given in the lang file
-     * locale sould be set
+     * 'SilverShop\Generic.DateTimeFormatNice'. Defaults to '%m/%d/%G %I:%M%p'
+     *
+     * @return string|null
      */
     public function Nice()
     {
         if ($this->value) {
-            return strftime(_t('SilverShop\Generic.DateTimeFormatNice', '%m/%d/%G %I:%M%p'), $this->owner->getTimestamp());
+            return strftime(_t('SilverShop\Generic.DateTimeFormatNice', '%m/%d/%G %I:%M%p'), $this->getTimestamp());
         }
     }
 
+    /**
+     * Returns the date in the format given in the lang file.
+     * 'SilverShop\Generic.DateFormatNice'. Defaults to '%m/%d/%G'
+     *
+     * @return string|null
+     */
     public function NiceDate()
     {
         if ($this->value) {
-            return strftime(_t('SilverShop\Generic.DateFormatNice', '%m/%d/%G'), $this->owner->getTimestamp());
+            return strftime(_t('SilverShop\Generic.DateFormatNice', '%m/%d/%G'), $this->getTimestamp());
         }
     }
 
+    /**
+     * Returns the 24h datetime in the format given in the lang file.
+     * 'SilverShop\Generic.DateTimeFormatNice24'. Defaults to 'd/m/Y H:i'
+     *
+     * @return string|null
+     */
     public function Nice24()
     {
-        return date(_t('SilverShop\Generic.DateTimeFormatNice24', 'd/m/Y H:i'), $this->owner->getTimestamp());
+        return date(_t('SilverShop\Generic.DateTimeFormatNice24', 'd/m/Y H:i'), $this->getTimestamp());
     }
 }
