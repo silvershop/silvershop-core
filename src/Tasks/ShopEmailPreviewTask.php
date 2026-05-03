fa5e28b557b3aa77e0a55927569878949bf1e86c
Will Rossiter <will@fullscreen.io>
2026-05-04 06:55:13 +1200

    allow previewing of OrderStatusLog Emails (#559)

--- src/Tasks/ShopEmailPreviewTask.php
+++ src/Tasks/ShopEmailPreviewTask.php
@@ -6,6 +6,7 @@ namespace SilverShop\Tasks;
 
 use SilverShop\Checkout\OrderEmailNotifier;
 use SilverShop\Model\Order;
+use SilverShop\Model\OrderStatusLog;
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\Control\Email\Email;
 use SilverStripe\PolyExecution\PolyOutput;
@@ -27,12 +28,16 @@ class ShopEmailPreviewTask extends BuildTask
 
     protected static string $description = 'Previews shop emails';
 
-    protected $previewableEmails = [
+    /**
+     * @var list<string>
+     */
+    protected array $previewableEmails = [
         'Confirmation',
         'Receipt',
         'AdminNotification',
         'CancelNotification',
-        'StatusChange'
+        'StatusChange',
+        'OrderStatusLog',
     ];
 
     public function getOptions(): array
@@ -42,7 +47,14 @@ class ShopEmailPreviewTask extends BuildTask
                 'email',
                 null,
                 InputOption::VALUE_OPTIONAL,
-                'The email type to preview. Available: Confirmation, Receipt, AdminNotification, CancelNotification, StatusChange',
+                'The email type to preview. Available: ' . implode(', ', $this->previewableEmails),
+            ),
+            new InputOption(
+                'order-status-log',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'SilverShop_OrderStatusLog ID to preview (optional). Only used when --email=OrderStatusLog; '
+                    . 'defaults to the latest customer-visible log for the preview order.',
             ),
         ];
     }
@@ -57,7 +69,7 @@ class ShopEmailPreviewTask extends BuildTask
         }
         $output->writeln('');
 
-        if ($email && in_array($email, $this->previewableEmails)) {
+        if ($email && in_array($email, $this->previewableEmails, true)) {
             $order = Order::get()->filter('Email:not', ['', null])->first()
                 ?? Order::get()->first();
 
@@ -74,11 +86,32 @@ class ShopEmailPreviewTask extends BuildTask
             $notifier = OrderEmailNotifier::create($order);
             $notifier->setDebugMode(true);
 
-            $method = 'send' . $email;
-
             if ($email === 'StatusChange') {
-                $output->writeForHtml($notifier->$method('This is a test title', 'This is a test note'));
+                $output->writeForHtml($notifier->sendStatusChange('This is a test title', 'This is a test note'));
+            } elseif ($email === 'OrderStatusLog') {
+                $logOption = $input->getOption('order-status-log');
+                $usedExplicitId = false;
+                $log = $this->resolvePreviewOrderStatusLog($order, $logOption, $usedExplicitId);
+
+                if (!$log) {
+                    if ($usedExplicitId) {
+                        $output->writeln(
+                            'Invalid order-status-log ID, or that log does not belong to the preview order.'
+                        );
+                    } else {
+                        $output->writeln(
+                            'No suitable OrderStatusLog found for this order. Create a visible log entry in the CMS, '
+                                . 'or pass --order-status-log=<ID> for a log that belongs to the preview order.'
+                        );
+                    }
+
+                    return Command::FAILURE;
+                }
+
+                $output->writeln(sprintf('Preview using OrderStatusLog #%d (%s)', $log->ID, $log->Title ?: '(no title)'));
+                $output->writeForHtml($notifier->sendStatusChange($log->Title ?: 'Order Update', (string) $log->Note));
             } else {
+                $method = 'send' . $email;
                 $output->writeForHtml($notifier->$method());
             }
             $output->writeForAnsi('Email rendered (HTML only - open in browser to preview).');
@@ -88,5 +121,45 @@ class ShopEmailPreviewTask extends BuildTask
         }
 
         return Command::SUCCESS;
+    }
+
+    /**
+     * @param mixed $logOption Raw value from {@link InputInterface::getOption()}
+     */
+    protected function resolvePreviewOrderStatusLog(Order $order, mixed $logOption, bool &$usedExplicitId): ?OrderStatusLog
+    {
+        $usedExplicitId = false;
+
+        if ($logOption !== null && $logOption !== '' && $logOption !== false) {
+            $usedExplicitId = true;
+            $id = (int) $logOption;
+            if ($id < 1) {
+                return null;
+            }
+
+            $log = OrderStatusLog::get()->byID($id);
+            if (!$log || (int) $log->OrderID !== (int) $order->ID) {
+                return null;
+            }
+
+            return $log;
+        }
+
+        $visible = OrderStatusLog::get()
+            ->filter([
+                'OrderID' => $order->ID,
+                'VisibleToCustomer' => true,
+            ])
+            ->sort('"Created" DESC')
+            ->first();
+
+        if ($visible) {
+            return $visible;
+        }
+
+        return OrderStatusLog::get()
+            ->filter('OrderID', $order->ID)
+            ->sort('"Created" DESC')
+            ->first();
     }
 }
